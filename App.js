6dd8b3af243a024eb605064415cbb6c0173ce157
Fahmi Nuralamsah <nuralamsah.fahmi@gmail.com>
2020-06-05 20:20:42 +0700

    DoneWithIt

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Platform } from 'react-native';
 
 export default function App() {
-  let x = 1;
+  const handlePress = () => console.log("Text pressed!")
 
   return (
-    <View style={styles.container}>
-      <Text>Hello World !</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>
+        React Native  - I will make it really really longer and see what will happend !
+      </Text>
+    </SafeAreaView>
   );
 }
 
@@ -15,7 +17,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? 25 : 0
   },
 });
